09c61ff4b49c46ee1931a26811fb36e93736caa6
dai-1103 <bosatsunumnum@gmail.com>
2022-03-19 22:56:03 +0900

    create project

---
+++ src/main/java/net/kunmc/lab/tntrain/TNTRain.java
@@ -0,0 +1,14 @@
+package net.kunmc.lab.tntrain;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TNTRain extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+  }
+
+  @Override
+  public void onDisable() {
+  }
+}
